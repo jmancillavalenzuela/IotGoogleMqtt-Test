24d33755621942f165a81dee30a0347020fd3e29
Juan <juan.mancilla.94@gmail.com>
2021-04-12 18:10:19 -0400

    First Update Main.ino

---
+++ includes/ciotc_config.h
@@ -0,0 +1,61 @@
+// Cloud IoT details.
+const char *project_id = "feisty-mechanic-310318";
+const char *location = "us-central1";
+const char *registry_id = "iotcore-registry";
+const char *device_id = "hs-1";
+
+// Configuration for NTP
+const char *ntp_primary = "pool.ntp.org";
+const char *ntp_secondary = "time.nist.gov";
+
+// Wifi newtork details.
+const char *ssid = "Mancilla";
+const char *password = "Valdivia4288";
+
+const char *private_key_str =
+  "69:6e:ba:b1:05:af:74:b8:d4:73:17:e0:92:0c:72:"
+  "dd:6b:db:71:7b:cc:a9:40:d6:60:8f:8b:8f:b1:68:"
+  "2f:7a";
+
+// Time (seconds) to expire token += 20 minutes for drift
+const int jwt_exp_secs = 3600; // Maximum 24H (3600*24)
+
+// In case we ever need extra topics
+const int ex_num_topics = 0;
+const char *ex_topics[ex_num_topics];
+// "/devices/my-device/tbd/#";
+//const int ex_num_topics = 1;
+//const char* ex_topics[ex_num_topics] = {
+//  "/devices/my-device/tbd/#"
+//};
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 13
+#endif
+
+const char *root_cert =
+"-----BEGIN CERTIFICATE-----\n"
+"MIIBxTCCAWugAwIBAgINAfD3nVndblD3QnNxUDAKBggqhkjOPQQDAjBEMQswCQYD\n"
+"VQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZpY2VzIExMQzERMA8G\n"
+"A1UEAxMIR1RTIExUU1IwHhcNMTgxMTAxMDAwMDQyWhcNNDIxMTAxMDAwMDQyWjBE\n"
+"MQswCQYDVQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZpY2VzIExM\n"
+"QzERMA8GA1UEAxMIR1RTIExUU1IwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAATN\n"
+"8YyO2u+yCQoZdwAkUNv5c3dokfULfrA6QJgFV2XMuENtQZIG5HUOS6jFn8f0ySlV\n"
+"eORCxqFyjDJyRn86d+Iko0IwQDAOBgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUw\n"
+"AwEB/zAdBgNVHQ4EFgQUPv7/zFLrvzQ+PfNA0OQlsV+4u1IwCgYIKoZIzj0EAwID\n"
+"SAAwRQIhAPKuf/VtBHqGw3TUwUIq7TfaExp3bH7bjCBmVXJupT9FAiBr0SmCtsuk\n"
+"miGgpajjf/gFigGM34F9021bCWs1MbL0SA==\n"
+"-----END CERTIFICATE-----\n"
+"-----BEGIN CERTIFICATE-----\n"
+"MIIB4TCCAYegAwIBAgIRKjikHJYKBN5CsiilC+g0mAIwCgYIKoZIzj0EAwIwUDEk\n"
+"MCIGA1UECxMbR2xvYmFsU2lnbiBFQ0MgUm9vdCBDQSAtIFI0MRMwEQYDVQQKEwpH\n"
+"bG9iYWxTaWduMRMwEQYDVQQDEwpHbG9iYWxTaWduMB4XDTEyMTExMzAwMDAwMFoX\n"
+"DTM4MDExOTAzMTQwN1owUDEkMCIGA1UECxMbR2xvYmFsU2lnbiBFQ0MgUm9vdCBD\n"
+"QSAtIFI0MRMwEQYDVQQKEwpHbG9iYWxTaWduMRMwEQYDVQQDEwpHbG9iYWxTaWdu\n"
+"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEuMZ5049sJQ6fLjkZHAOkrprlOQcJ\n"
+"FspjsbmG+IpXwVfOQvpzofdlQv8ewQCybnMO/8ch5RikqtlxP6jUuc6MHaNCMEAw\n"
+"DgYDVR0PAQH/BAQDAgEGMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFFSwe61F\n"
+"uOJAf/sKbvu+M8k8o4TVMAoGCCqGSM49BAMCA0gAMEUCIQDckqGgE6bPA7DmxCGX\n"
+"kPoUVy0D7O48027KqGx2vKLeuwIgJ6iFJzWbVsaj8kfSt24bAgAXqmemFZHe+pTs\n"
+"ewv4n4Q=\n"
+"-----END CERTIFICATE-----\n";
---
+++ src/main.ino
@@ -0,0 +1,25 @@
+#include "esp32-mqtt.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  setupCloudIoT();
+}
+
+unsigned long lastMillis = 0;
+void loop() {
+  mqtt->loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+
+  if (!mqttClient->connected()) {
+    connect();
+  }
+
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    publishTelemetry("/iotcore-topic", "Hello123");
+    Serial.println("datos enviados");
+  }
+}
